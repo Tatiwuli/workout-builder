c08a07d90463090f4b930e7160aba9f2fa2bbaf7
Tatiwuli <innywuli@gmail.com>
2025-01-15 12:15:37 +0900

    Edited sys path

--- frontend/Home.py
+++ frontend/Home.py
@@ -1,12 +1,14 @@
 
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
-from frontend.utils import render_nav_link, render_logout
-from streamlit_cookies_manager import EncryptedCookieManager
 import os
 from dotenv import load_dotenv
 import sys
 import streamlit as st
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+from frontend.utils import render_nav_link, render_logout
+from streamlit_cookies_manager import EncryptedCookieManager
+
 st.set_page_config(page_title="Workout Builder",
                    page_icon="💪", layout="centered")
 
