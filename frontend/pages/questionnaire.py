fe2b4a602710a93ea8471ca7968943b3526c7ebc
Tatiwuli <innywuli@gmail.com>
2025-01-17 14:40:56 +0900

    Add the parent directory of rontend to sys.path

--- frontend/pages/questionnaire.py
+++ frontend/pages/questionnaire.py
@@ -3,8 +3,10 @@
 
 import streamlit as st
 import time
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "..")))
+import sys
+import os 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from frontend.run_generate_workout_plan import trigger_generate_workout_plan
 from frontend.questionnaire_utils import goal_question, frequency_question, duration_question, experience_question
